4592b86007dc33a0513b3dc0a95438c85e0a09c4
Julio César Solís Jímenez <julio20092003@hotmail.com>
2024-08-22 01:42:37 -0600

    Add project files.

---
+++ GymControlAPP/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using GymControlAPP.Views;
+namespace GymControlAPP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+    }
+}
+
